83dc55d138e0370a6771a060b04afa953f3d8aec
DevExpressExampleBot <fakeEmail>
2022-05-19 15:01:23 +0400

    Source auto update [skip ci]

--- VB/WinFormsPivotGridDataFieldsExample/Form1.Designer.vb
+++ VB/WinFormsPivotGridDataFieldsExample/Form1.Designer.vb
@@ -1,57 +1,56 @@
-﻿Namespace WinFormsPivotGridDataFieldsExample
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace WinFormsPivotGridDataFieldsExample
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
-			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			'
-			'pivotGridControl1
-			'
-			Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.pivotGridControl1.Name = "pivotGridControl1"
-			Me.pivotGridControl1.OptionsData.DataProcessingEngine = DevExpress.XtraPivotGrid.PivotDataProcessingEngine.Optimized
-			Me.pivotGridControl1.Size = New System.Drawing.Size(808, 450)
-			Me.pivotGridControl1.TabIndex = 0
-			'
-			'Form1
-			'
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(808, 450)
-			Me.Controls.Add(Me.pivotGridControl1)
-			Me.Name = "Form1"
-			Me.Text = "PivotGrid Example"
-			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' pivotGridControl1
+            ' 
+            Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.pivotGridControl1.Name = "pivotGridControl1"
+            Me.pivotGridControl1.OptionsData.DataProcessingEngine = DevExpress.XtraPivotGrid.PivotDataProcessingEngine.Optimized
+            Me.pivotGridControl1.Size = New System.Drawing.Size(808, 450)
+            Me.pivotGridControl1.TabIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(808, 450)
+            Me.Controls.Add(Me.pivotGridControl1)
+            Me.Name = "Form1"
+            Me.Text = "PivotGrid Example"
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
 #End Region
-
-		Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-	End Class
+        Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
+    End Class
 End Namespace
-
--- VB/WinFormsPivotGridDataFieldsExample/Form1.vb
+++ VB/WinFormsPivotGridDataFieldsExample/Form1.vb
@@ -1,69 +1,40 @@
-﻿Imports DevExpress.DataAccess.Excel
+Imports DevExpress.DataAccess.Excel
 Imports DevExpress.XtraEditors
 Imports DevExpress.XtraPivotGrid
 Imports System
 
 Namespace WinFormsPivotGridDataFieldsExample
-	Partial Public Class Form1
-		Inherits XtraForm
 
-		Public Sub New()
-			InitializeComponent()
-			AddHandler Me.Load, AddressOf Form1_Load
-			' Create the Excel Data Source.
-			Dim ds As New ExcelDataSource()
-			ds.FileName = "SalesPerson.xlsx"
-			Dim settings As New ExcelWorksheetSettings("Data")
-			ds.SourceOptions = New ExcelSourceOptions(settings)
-			ds.Fill()
-			' Set the pivot's data source.
-			pivotGridControl1.DataSource = ds
-			' Create pivot grid fields.
-			Dim fieldCategoryName As New PivotGridField() With {
-				.Area = PivotArea.RowArea,
-				.AreaIndex = 0,
-				.Caption = "Category Name",
-				.DataBinding = New DataSourceColumnBinding("CategoryName")
-			}
-			Dim fieldProductName As New PivotGridField() With {
-				.Area = PivotArea.RowArea,
-				.AreaIndex = 1,
-				.Caption = "Product Name",
-				.DataBinding = New DataSourceColumnBinding("ProductName")
-			}
-			Dim fieldExtendedPrice As New PivotGridField() With {
-				.Area = PivotArea.DataArea,
-				.AreaIndex = 0,
-				.Caption = "Extended Price",
-				.DataBinding = New DataSourceColumnBinding("Extended Price")
-			}
-			' Specify the field format.
-			fieldExtendedPrice.CellFormat.FormatType = DevExpress.Utils.FormatType.Numeric
-			fieldExtendedPrice.CellFormat.FormatString = "c2"
+    Public Partial Class Form1
+        Inherits XtraForm
 
-			Dim fieldOrderDate1 As New PivotGridField() With {
-				.Area = PivotArea.ColumnArea,
-				.AreaIndex = 0,
-				.Caption = "Year",
-				.DataBinding = New DataSourceColumnBinding("OrderDate", PivotGroupInterval.DateYear)
-			}
-			Dim fieldOrderDate2 As New PivotGridField() With {
-				.Area = PivotArea.ColumnArea,
-				.AreaIndex = 1,
-				.Caption = "Quarter",
-				.DataBinding = New DataSourceColumnBinding("OrderDate", PivotGroupInterval.DateQuarter)
-			}
-			Dim fieldCountry As New PivotGridField() With {
-				.AreaIndex = 0,
-				.Caption = "Country",
-				.DataBinding = New DataSourceColumnBinding("Country")
-			}
-			' Add fields to the pivot grid.
-			pivotGridControl1.Fields.AddRange(New PivotGridField() { fieldCategoryName, fieldProductName, fieldOrderDate1, fieldOrderDate2, fieldExtendedPrice, fieldCountry})
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            AddHandler Load, AddressOf Form1_Load
+            ' Create the Excel Data Source.
+            Dim ds As ExcelDataSource = New ExcelDataSource()
+            ds.FileName = "SalesPerson.xlsx"
+            Dim settings As ExcelWorksheetSettings = New ExcelWorksheetSettings("Data")
+            ds.SourceOptions = New ExcelSourceOptions(settings)
+            ds.Fill()
+            ' Set the pivot's data source.
+            pivotGridControl1.DataSource = ds
+            ' Create pivot grid fields.
+            Dim fieldCategoryName As PivotGridField = New PivotGridField() With {.Area = PivotArea.RowArea, .AreaIndex = 0, .Caption = "Category Name", .DataBinding = New DataSourceColumnBinding("CategoryName")}
+            Dim fieldProductName As PivotGridField = New PivotGridField() With {.Area = PivotArea.RowArea, .AreaIndex = 1, .Caption = "Product Name", .DataBinding = New DataSourceColumnBinding("ProductName")}
+            Dim fieldExtendedPrice As PivotGridField = New PivotGridField() With {.Area = PivotArea.DataArea, .AreaIndex = 0, .Caption = "Extended Price", .DataBinding = New DataSourceColumnBinding("Extended Price")}
+            ' Specify the field format.
+            fieldExtendedPrice.CellFormat.FormatType = DevExpress.Utils.FormatType.Numeric
+            fieldExtendedPrice.CellFormat.FormatString = "c2"
+            Dim fieldOrderDate1 As PivotGridField = New PivotGridField() With {.Area = PivotArea.ColumnArea, .AreaIndex = 0, .Caption = "Year", .DataBinding = New DataSourceColumnBinding("OrderDate", PivotGroupInterval.DateYear)}
+            Dim fieldOrderDate2 As PivotGridField = New PivotGridField() With {.Area = PivotArea.ColumnArea, .AreaIndex = 1, .Caption = "Quarter", .DataBinding = New DataSourceColumnBinding("OrderDate", PivotGroupInterval.DateQuarter)}
+            Dim fieldCountry As PivotGridField = New PivotGridField() With {.AreaIndex = 0, .Caption = "Country", .DataBinding = New DataSourceColumnBinding("Country")}
+            ' Add fields to the pivot grid.
+            pivotGridControl1.Fields.AddRange(New PivotGridField() {fieldCategoryName, fieldProductName, fieldOrderDate1, fieldOrderDate2, fieldExtendedPrice, fieldCountry})
+        End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
-			pivotGridControl1.BestFit()
-		End Sub
-	End Class
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            pivotGridControl1.BestFit()
+        End Sub
+    End Class
 End Namespace
